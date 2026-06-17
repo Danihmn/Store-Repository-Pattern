fd2854fe7dc494b7ee019b7a64f949d8c4be1d58
Danihmn <daniel.bezerra.mult@gmail.com>
2026-06-16 22:38:52 -0300

    Adiciona repositório de CRUD dos produtos

---
+++ Store/Repositories/ProdutoRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Data;
+using Store.Models;
+
+namespace Store.Repositories;
+
+public class ProdutoRepository (StoreContext context)
+{
+    public async Task<List<Produto>?> GetAllAsync (int skip = 0, int take = 10, CancellationToken cancellationToken = default)
+        => await context.Produtos
+            .AsNoTracking()
+            .Skip(skip)
+            .Take(take)
+            .ToListAsync(cancellationToken);
+
+    public async Task<Produto?> GetByIdAsync (Guid id, CancellationToken cancellationToken = default)
+        => await context.Produtos
+            .AsNoTracking()
+            .FirstOrDefaultAsync(p => p.Id == id, cancellationToken);
+
+    public async Task<Produto> CreateAsync (Produto produto, CancellationToken cancellationToken = default)
+    {
+        await context.AddAsync(produto, cancellationToken);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return produto;
+    }
+
+    public async Task<Produto> UpdateAsync (Produto produto, CancellationToken cancellationToken = default)
+    {
+        context.Update(produto);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return produto;
+    }
+
+    public async Task<Produto> DeleteAsync (Produto produto, CancellationToken cancellationToken = default)
+    {
+        context.Remove(produto);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return produto;
+    }
+}
